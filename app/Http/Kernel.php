48ce9a4f80c69ee5663ab4f8459c2494a55e40af
Erick Cordeiro <erickcordeiroa@gmail.com>
2022-05-18 14:50:18 -0300

    Install Breeze Autenthicate

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'company' => \App\Http\Middleware\CheckCompanyMiddleware::class,
+        'candidate' => \App\Http\Middleware\CheckCandidateMiddleware::class,
     ];
 }
